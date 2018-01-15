c9a2f41cdf880fd336ce6b1f58102ceed52b35ca
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-15 15:53:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+let userKeyPress = [];
 function init() {
   // Write your JavaScript code inside the init() function
   const body = document.querySelector('body')
- 
+
 input.addEventListener('keydown', function(e) {
-  console.log(e.which)
+  console.log(e.which);
+  userKeyPress.push(e.which);
+  console.log(userKeyPress);
 });
 }
